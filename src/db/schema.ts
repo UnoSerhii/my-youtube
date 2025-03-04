9a9934d4198ab9986761b29fe191b7c2f7d26d4c
UnoSerhii <you@example.com>
2025-03-04 13:28:58 +0200

    fix url_addres

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -4,7 +4,7 @@ export const users = pgTable("users", {
   id: uuid("id").primaryKey().defaultRandom(),
   clerkId: text("clerk_id").unique().notNull(),
   name: text("name").notNull(),
-  imageIrl: text("image_url").notNull(),
+  imageUrl: text("image_url").notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 }, (t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)]);
